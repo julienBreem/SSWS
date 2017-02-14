a210b8e58a43e0b43d862f4507c3fd94769b53c8
julienBreem <julienbreem@gmail.com>
2017-02-14 19:22:10 +0100

    prettyUrlDisable

--- config/web.php
+++ config/web.php
@@ -42,14 +42,14 @@ $config = [
         ],
         'db' => require(__DIR__ . '/db.php'),
 
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'enableStrictParsing' => true,
-            'showScriptName' => false,
-            'rules' => [
-                ['class' => 'yii\rest\UrlRule', 'controller' => 'spot'],
-            ],
-        ],
+        // 'urlManager' => [
+            // 'enablePrettyUrl' => true,
+            // 'enableStrictParsing' => true,
+            // 'showScriptName' => false,
+            // 'rules' => [
+                // ['class' => 'yii\rest\UrlRule', 'controller' => 'spot'],
+            // ],
+        // ],
 
     ],
     'params' => $params,
